7d8c1df5b28d64d310358ab856b16c5fcce9a493
Ameya S <ameyasub@gmail.com>
2025-08-06 23:56:36 -0400

    added 3 features after testing in a notebook

--- features/features.py
+++ features/features.py
@@ -10,3 +10,10 @@ FEATURES = ["sma_20", "ema_20", "log_return", "rolling_std_20", "rsi_14",
 # log_return: log difference of consecutive Close prices
 # rolling_std_10: 10-day rolling standard deviation (volatility)
 # lag_1: yesterday's Close
+
+def add_features(df):
+    df["sma_20"] = df["Close"].rolling(20).mean() # .rolling() executes taking the sum of prices of the last 20 days
+    df["ema_20"] = df["Close"].ewm(span=20).mean() # ewm does exponential weighting
+    s = pd.Series(df["Close"].shift(1)) # lines up yesterday's close with today
+    df["log_return"] = np.log(df["Close"].div(s)) # log difference of yesterday and today's close
+    return df
